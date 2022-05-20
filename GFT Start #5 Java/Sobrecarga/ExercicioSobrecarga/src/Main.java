c1128b596b39b35ec0e75a564a57f76559f00918
unknown <queiroz2@gmail.com>
2022-05-19 23:54:24 -0300

    update exercicio sobrecarga

---
+++ GFT Start #5 Java/Sobrecarga/ExercicioSobrecarga/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args)  {
+
+        System.out.println("Exercício Sobrecarga de Método");
+        Quadrilatero.area(3);
+        Quadrilatero.area(4, 5);
+    
+    }
+}
---
+++ GFT Start #5 Java/Sobrecarga/ExercicioSobrecarga/src/Quadrilatero.java
@@ -0,0 +1,12 @@
+public class Quadrilatero {
+
+    //Cálculo da área de um quadrado
+    public static void area(double lado) {
+        System.out.println("Área do quadrado: " + lado * lado);
+    }
+    
+    //Cálculo da área do retângulo
+    public static void area(double lado1, double lado2) {
+        System.out.println("Área do retângulo: " + lado1 * lado2);
+    }
+}
